5f3e91bd399c08f4a961564e227714b422fef709
iron-hope-shop <me@brad-jackson.com>
2023-09-25 20:42:41 -0500

    refactor to optimize cost

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ from flask import Flask, jsonify
 from flask import request, abort
 import google_crc32c
 import openai
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+# Load NLTK stopwords
+STOPWORDS = set(stopwords.words("english"))
+
+
+# Fetch all required secrets at once
+def fetch_secrets():
+    secrets = [
+        "API_TYPE",
+        "API_BASE",
+        "API_VERSION",
+        "API_KEY",
+        "ENGINE",
+        "C1_UN",
+        "C2_PW",
+    ]
+    return {secret: access_secret_version(secret, "1") for secret in secrets}
+
+
+SECRETS = fetch_secrets()
+
+
+def remove_stopwords(text):
+    return " ".join([word for word in text.split() if word.lower() not in STOPWORDS])
 
 
 def access_secret_version(
@@ -36,22 +63,35 @@ def access_secret_version(
     payload = response.payload.data.decode("UTF-8")
     return payload
 
+
+def authenticate():
+    auth = request.authorization
+    if not auth or not (
+        auth.username == SECRETS["C1_UN"] and auth.password == SECRETS["C2_PW"]
+    ):
+        abort(401)
+
+
 # Set up OpenAI API configurations
 def setup_openai_api():
-    openai.api_type = access_secret_version("API_TYPE", "1")
-    openai.api_base = access_secret_version("API_BASE", "1")
-    openai.api_version = access_secret_version("API_VERSION", "1")
-    openai.api_key = access_secret_version("API_KEY", "1")
+    openai.api_type = SECRETS["API_TYPE"]
+    openai.api_base = SECRETS["API_BASE"]
+    openai.api_version = SECRETS["API_VERSION"]
+    openai.api_key = SECRETS["API_KEY"]
+
 
 def query(user_query, channel_history):
-    system_instruction = f"You are a Software Engineer. Your job is to write effective code in a pair programming environment with your teammate, Brad."
+    system_instruction = f"Answer briefly and to the point."
     # Starting with the system instruction
     messages = [{"role": "system", "content": system_instruction}]
-    
+
     # Convert and add the previous interactions from the history
     for interaction in channel_history:
         messages.append({"role": "user", "content": interaction["prompt"]})
         messages.append({"role": "assistant", "content": interaction["response"]})
+
+    # Remove stopwords from user query
+    user_query = remove_stopwords(user_query)
 
     # Add the current user query
     messages.append({"role": "user", "content": user_query})
@@ -62,37 +102,31 @@ def query(user_query, channel_history):
 
     # Asynchronous API call
     chat_completion_resp = openai.ChatCompletion.create(
-        engine=access_secret_version("ENGINE", "1"),
+        engine=SECRETS["ENGINE"],
         messages=messages,
-        temperature=0.7,
-        max_tokens=800,
+        temperature=0.95,
+        max_tokens=500,
         top_p=0.95,
         frequency_penalty=0,
         presence_penalty=0,
-        stop=None
+        stop=None,
     )
     return chat_completion_resp.choices[0].message.content
 
-c1_un = access_secret_version("C1_UN", "1")
-c2_pw = access_secret_version("C2_PW", "1")
 
-
-def authenticate():
-    auth = request.authorization
-    if not auth or not (auth.username == c1_un and auth.password == c2_pw):
-        abort(401)
 app = Flask(__name__)
 
-@app.route('/echo', methods=['POST'])
+
+@app.route("/echo", methods=["POST"])
 def echo():
     authenticate()  # Ensure the user is authenticated
     data = request.json
-    user_query = data.get('input', '')
-    channel_history = data.get('history', [])
+    user_query = data.get("input", "")
+    channel_history = data.get("history", [])
     response = query(user_query, channel_history)
     return jsonify({"response": response})
 
 
 if __name__ == "__main__":
     setup_openai_api()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
