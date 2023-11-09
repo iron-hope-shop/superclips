31280ab6e29fab923b528cae8ad9936d193c8a2a
iron-hope-shop <me@brad-jackson.com>
2023-11-08 21:10:19 -0600

    rollback

--- main.py
+++ main.py
@@ -17,36 +17,43 @@ client = secretmanager.SecretManagerServiceClient()
 #     response = client.access_secret_version(request={"name": name})
 #     return response.payload.data.decode("UTF-8")
 
-def access_secret_version(
-    secret_id: str
-) -> secretmanager.AccessSecretVersionResponse:
-    """
-    Access the payload for the given secret version if one exists. The version
-    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
-    """
+# def access_secret_version(
+#     secret_id: str
+# ) -> secretmanager.AccessSecretVersionResponse:
+#     """
+#     Access the payload for the given secret version if one exists. The version
+#     can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+#     """
 
-    # Import the Secret Manager client library.
-    from google.cloud import secretmanager
+#     # Import the Secret Manager client library.
+#     from google.cloud import secretmanager
 
-    # Create the Secret Manager client.
-    client = secretmanager.SecretManagerServiceClient()
+#     # Create the Secret Manager client.
+#     client = secretmanager.SecretManagerServiceClient()
 
-    # Build the resource name of the secret version.
-    name = f"projects/superclips/secrets/{secret_id}/versions/2"
+#     # Build the resource name of the secret version.
+#     name = f"projects/superclips/secrets/{secret_id}/versions/2"
 
-    # Access the secret version.
+#     # Access the secret version.
+#     response = client.access_secret_version(request={"name": name})
+
+#     # Verify payload checksum.
+#     crc32c = google_crc32c.Checksum()
+#     crc32c.update(response.payload.data)
+#     if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+#         print("Data corruption detected.")
+#         return response
+
+#     # WARNING: Do not print the secret in a production environment
+#     payload = response.payload.data.decode("UTF-8")
+#     return payload
+
+# Function to retrieve secrets
+def access_secret_version(secret_id):
+    project_id = os.environ.get('GCP_PROJECT')
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/2"
     response = client.access_secret_version(request={"name": name})
-
-    # Verify payload checksum.
-    crc32c = google_crc32c.Checksum()
-    crc32c.update(response.payload.data)
-    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-        print("Data corruption detected.")
-        return response
-
-    # WARNING: Do not print the secret in a production environment
-    payload = response.payload.data.decode("UTF-8")
-    return payload
+    return response.payload.data.decode("UTF-8")
 
 # Retrieve your Twitter OAuth2.0 credentials from Secret Manager
 TWITTER_CLIENT_ID = access_secret_version('twitter_oauth_client_id')
