1fbf22f4994e5c562ae8c38550853a60ce31b5ec
iron-hope-shop <me@brad-jackson.com>
2023-11-08 20:25:03 -0600

    added decryption... whoops

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, request, session, jsonify
 import os
 from google.cloud import secretmanager
 import requests
+import google_crc32c
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Ensure this is kept secret and use a persistent key in production
@@ -9,12 +10,43 @@ app.secret_key = os.urandom(24)  # Ensure this is kept secret and use a persiste
 # Initialize Secret Manager client
 client = secretmanager.SecretManagerServiceClient()
 
-# Function to retrieve secrets
-def access_secret_version(secret_id):
-    project_id = os.environ.get('GCP_PROJECT')
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/2"
+# # Function to retrieve secrets
+# def access_secret_version(secret_id):
+#     project_id = os.environ.get('GCP_PROJECT')
+#     name = f"projects/{project_id}/secrets/{secret_id}/versions/2"
+#     response = client.access_secret_version(request={"name": name})
+#     return response.payload.data.decode("UTF-8")
+
+def access_secret_version(
+    secret_id: str, version_id: str
+) -> secretmanager.AccessSecretVersionResponse:
+    """
+    Access the payload for the given secret version if one exists. The version
+    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+    """
+
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/superclips/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
     response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("UTF-8")
+
+    # Verify payload checksum.
+    crc32c = google_crc32c.Checksum()
+    crc32c.update(response.payload.data)
+    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+        print("Data corruption detected.")
+        return response
+
+    # WARNING: Do not print the secret in a production environment
+    payload = response.payload.data.decode("UTF-8")
+    return payload
 
 # Retrieve your Twitter OAuth2.0 credentials from Secret Manager
 TWITTER_CLIENT_ID = access_secret_version('twitter_oauth_client_id')
